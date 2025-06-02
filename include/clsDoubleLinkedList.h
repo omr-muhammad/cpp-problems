4ab9fd504318e7eb8edd828d063267383b57a3ec
unknown <omarelsayedmuhammad@gmail.com>
2025-06-02 14:46:19 +0300

    Implemented insertAtBeginning method

--- include/clsDoubleLinkedList.h
+++ include/clsDoubleLinkedList.h
@@ -12,14 +12,18 @@ using namespace std;
 template <typename T>
 class clsDoubleLinkedList
 {
+private:
+    int _size = 0;
+
 public:
     class Node
     {
     public:
         T value;
-        Node *next;
-        Node *prev;
+        Node *next = NULL;
+        Node *prev = NULL;
     };
+
     Node *head = NULL;
     Node *tail = NULL;
 
@@ -38,6 +42,27 @@ public:
             cout << cur->value << " ";
             cur = cur->next;
         }
+    }
+
+    void insertAtBeginning(const T &value)
+    {
+        Node *newNode = new Node;
+        newNode->value = value;
+        newNode->next = head;
+
+        if (head == NULL)
+        {
+            head = tail = newNode;
+            // [next, prev] are initially NULL
+        }
+        else
+        {
+            // This line handle One list node since head = tail = newNode at the first insertion
+            head->prev = newNode;
+            head = newNode;
+        }
+
+        _size++;
     }
 };
 #endif // CLS_DOUBLE_LINKED_LIST_H
